957ca03c9f19f3a556b03535f567d66685e3a1b8
tu1511 <131641671+tu1511@users.noreply.github.com>
2024-03-09 15:41:28 +0700

    setup code

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import HomePage from './pages/users/homePage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <h1>Green Food</h1>
+  <HomePage/>
 );
 
 
---
+++ src/pages/users/homePage/index.js
@@ -0,0 +1,14 @@
+import { memo } from "react";
+
+
+const HomePage = () => {
+    return (
+        <>
+        <h1>Header</h1>
+        <h1>Body</h1>
+        <h1>Footer</h1>
+        </>
+    );
+};
+
+export default memo(HomePage);
