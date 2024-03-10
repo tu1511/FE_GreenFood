99602b4cf9f58c2b97b4f7f3810985b827e27a7e
tu1511 <131641671+tu1511@users.noreply.github.com>
2024-03-10 22:37:15 +0700

    setup config

--- src/pages/users/theme/header/index.js
+++ src/pages/users/theme/header/index.js
@@ -1,10 +1,52 @@
 import { memo } from "react";
 import "./header.scss";
+import {
+  AiOutlineFacebook,
+  AiOutlineInstagram,
+  AiOutlineLinkedin,
+  AiOutlineGithub,
+  AiOutlineUser,
+} from "react-icons/ai";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <div className="header__top">
-      <div className="container">Header ne</div>
+      <div className="container">
+        <div className="row">
+          <div className="col-6">trai</div>
+          <div className="col-6 header__top_right">
+            <ul>
+              <li>
+                <Link to={""}>
+                  <AiOutlineFacebook />
+                </Link>
+              </li>
+              <li>
+                <Link to={""}>
+                  <AiOutlineInstagram />
+                </Link>
+              </li>
+              <li>
+                <Link to={""}>
+                  <AiOutlineLinkedin />
+                </Link>
+              </li>
+              <li>
+                <Link to={""}>
+                  <AiOutlineGithub />
+                </Link>
+              </li>
+              <li>
+                <Link to={""}>
+                  <AiOutlineUser />
+                </Link>
+                <span>Đăng nhập</span>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
