684adf49d9c5053a8a22a3c21d108bb060ff2ca1
tu1511 <131641671+tu1511@users.noreply.github.com>
2024-03-10 23:54:48 +0700

    set layout

--- src/pages/users/theme/header/header.js
+++ src/pages/users/theme/header/header.js
@@ -13,52 +13,62 @@ import { formatter } from "utils/formatter";
 
 const Header = () => {
   return (
-    <div className="header__top">
-      <div className="container">
-        <div className="row">
-          <div className="col-6 header__top_left">
-            <ul>
-              <li>
-                {" "}
-                <AiOutlineMail />
-                minhtu15112K3@gmail.com
-              </li>
-              <li>Miễn phí ship đơn từ {formatter(150000)}</li>
-            </ul>
-          </div>
-          <div className="col-6 header__top_right">
-            <ul>
-              <li>
-                <Link to={""}>
-                  <AiOutlineFacebook />
-                </Link>
-              </li>
-              <li>
-                <Link to={""}>
-                  <AiOutlineInstagram />
-                </Link>
-              </li>
-              <li>
-                <Link to={""}>
-                  <AiOutlineLinkedin />
-                </Link>
-              </li>
-              <li>
-                <Link to={""}>
-                  <AiOutlineGithub />
-                </Link>
-              </li>
-              <li>
-                <Link to={""}>
-                  <AiOutlineUser />
-                </Link>
-                <span>Đăng nhập</span>
-              </li>
-            </ul>
+    <>
+      <div className="header__top">
+        <div className="container">
+          <div className="row">
+            <div className="col-6 header__top_left">
+              <ul>
+                <li>
+                  {" "}
+                  <AiOutlineMail />
+                  minhtu15112K3@gmail.com
+                </li>
+                <li>Miễn phí ship đơn từ {formatter(150000)}</li>
+              </ul>
+            </div>
+            <div className="col-6 header__top_right">
+              <ul>
+                <li>
+                  <Link to={""}>
+                    <AiOutlineFacebook />
+                  </Link>
+                </li>
+                <li>
+                  <Link to={""}>
+                    <AiOutlineInstagram />
+                  </Link>
+                </li>
+                <li>
+                  <Link to={""}>
+                    <AiOutlineLinkedin />
+                  </Link>
+                </li>
+                <li>
+                  <Link to={""}>
+                    <AiOutlineGithub />
+                  </Link>
+                </li>
+                <li>
+                  <Link to={""}>
+                    <AiOutlineUser />
+                  </Link>
+                  <span>Đăng nhập</span>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+
+      <div className="container">
+        <div className="row">
+          <div className=" col-xl-3">LOGO</div>
+          <div className=" col-xl-6">MENU</div>
+          <div className=" col-xl-3">PHONE</div>
+        </div>
+      </div>
+    </>
   );
 };
 
